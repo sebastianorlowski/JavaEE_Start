8a85d2fc7e58dddaa2ce659e10a5e7fe5caf76e0
Sebastian Orlowski <sebastian.orlowski420@gmail.com>
2020-06-21 16:16:02 +0200

    CreateCookie

--- src/pl/orlowskisebastian/javaeehelloworld/HelloWorldServlet.java
+++ src/pl/orlowskisebastian/javaeehelloworld/HelloWorldServlet.java
@@ -1,6 +1,7 @@
 package pl.orlowskisebastian.javaeehelloworld;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,6 +14,15 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter pw = response.getWriter();
+        if (request.getCookies() != null)
+            for (int i = 0; i < request.getCookies().length; i++) {
+                Cookie c = request.getCookies()[i];
+                if (c.getName().equals("imie")) {
+                    pw.println("Witaj, " + c.getValue());
+                    return;
+            }
+        }
+
         pw.println("<html><head><title>Pierwsza aplikacja webowa</title></head>");
         pw.println("<body>");
         pw.println("<h2>Hello, world!</h2>");
@@ -30,5 +40,9 @@ public class HelloWorldServlet extends HttpServlet {
         String imie = request.getParameter("imie");
         if (imie != null && !"".equals(imie))
             pw.println("Witaj, " + imie);
+            Cookie ciastko = new Cookie("imie", imie);
+            ciastko.setMaxAge(3600);
+            response.addCookie(ciastko);
+
     }
 }
