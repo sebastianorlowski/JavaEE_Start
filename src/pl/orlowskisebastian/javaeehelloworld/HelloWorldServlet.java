ea5744ece1561c8b8fe95e36e21b4d63d393430a
Sebastian Orlowski <sebastian.orlowski420@gmail.com>
2020-06-21 16:39:40 +0200

    CreateSession

--- src/pl/orlowskisebastian/javaeehelloworld/HelloWorldServlet.java
+++ src/pl/orlowskisebastian/javaeehelloworld/HelloWorldServlet.java
@@ -1,10 +1,6 @@
 package pl.orlowskisebastian.javaeehelloworld;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -14,19 +10,29 @@ public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter pw = response.getWriter();
-        if (request.getCookies() != null)
+        /*if (request.getCookies() != null)
             for (int i = 0; i < request.getCookies().length; i++) {
                 Cookie c = request.getCookies()[i];
                 if (c.getName().equals("imie")) {
                     pw.println("Witaj, " + c.getValue());
                     return;
             }
+        }*/
+
+        HttpSession sesja = request.getSession();
+        Integer licznik = (Integer)sesja.getAttribute("licznik");
+        if (licznik == null) {
+            licznik = 1;
+        } else {
+            licznik++;
         }
+        sesja.setAttribute("licznik", licznik);
 
         pw.println("<html><head><title>Pierwsza aplikacja webowa</title></head>");
         pw.println("<body>");
         pw.println("<h2>Hello, world!</h2>");
         pw.println("Second line");
+        pw.println("Licznik odwiedzin: " + licznik);
         pw.println("<form method=\"post\">");
         pw.println("<p>Podaj imię: <p><input name=\"imie\" / ><input type =\"submit\" value=\"OK\" />");
         pw.println("</form>");
